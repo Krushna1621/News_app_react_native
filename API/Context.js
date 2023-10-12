479043460fe391fdb8bb9a92fb8878a71c2e215b
Krushna1621 <shindekrushna825@gmail.com>
2023-10-12 15:50:27 +0530

    first commit

---
+++ API/Context.js
@@ -0,0 +1,12 @@
+import { createContext } from "react";
+
+export const NewsContext= createContext()
+
+const Context=({children})=>{
+    return(
+        <Context.Provider>
+
+        </Context.Provider>
+    )
+}
+export default Context;
